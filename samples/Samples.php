18c819250d671a1cfd781b212b6f69acb91847cc
Sergey <der.fremd@gmail.com>
2022-09-14 14:03:32 +0300

    Added source code highlight in samples.

--- samples/Samples.php
+++ samples/Samples.php
@@ -18,7 +18,7 @@ if(!isset($test_id) || !isset($tests[$test_id])) {
 
 echo "<h3>Sample name: $tests[$test_id]</h3>";
 
-echo "Source<hr><pre><code>" . htmlspecialchars(file_get_contents(__DIR__ . '/' .$tests[$test_id])) .
+echo "Source<hr><pre><code>" . highlight_file(__DIR__ . '/' .$tests[$test_id], true) .
     "\n// End of source " . $tests[$test_id] ."</code></pre>\n";
 
 echo '<hr>Begin of sample result<hr>';
